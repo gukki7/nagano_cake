b4d7cf3ca7a4b58615c9b5a8acd136f21f124a16
EC2 Default User <duffy.may08@outlook.jp>
2022-07-02 13:59:36 +0000

    [ADD] マイグレーションファイルの編集

--- db/migrate/20220701142849_create_order_details.rb
+++ db/migrate/20220701142849_create_order_details.rb
@@ -1,7 +1,9 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
-
+      t.integer :price, null: false
+      t.integer :amount, null: false
+      t.integer :making_status, null: false
       t.timestamps
     end
   end
