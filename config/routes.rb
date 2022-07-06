c5e3bb030e5b816cf4f487c512b2250ccfeb04ed
EC2 Default User <duffy.may08@outlook.jp>
2022-07-06 14:46:33 +0000

    [ADD] マイグレーションファイルの追加・削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :customers
   namespace :admin do
     root to: 'homes#top'
-    resources :sessions, only: [:new, :create, :destroy]
+    #resources :sessions, only: [:new, :create, :destroy]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
-  
+
   namespace :public do
     root to: 'homes#top'
     get 'home/about' => 'homes#about'
@@ -18,8 +19,8 @@ Rails.application.routes.draw do
     get 'orders/comfirm' => 'orders/comfirm'
     get 'orders/complete' => 'orders/complete'
     resources :items, only: [:index, :show,]
-    resources :registrations, only: [:new, :create]
-    resources :sessions, only: [:new, :create, :destroy]
+   # resources :registrations, only: [:new, :create]
+   # resources :sessions, only: [:new, :create, :destroy]
     resources :customers, only: [:show, :edit, :update]
     resources :cart_items, only: [:index, :create, :update, :destroy]
     resources :orders, only: [:index, :new, :create, :show]
