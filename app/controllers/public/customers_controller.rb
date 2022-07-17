349ec8e724c16294a3715070ced892625c6a6a63
EC2 Default User <duffy.may08@outlook.jp>
2022-07-17 14:49:49 +0000

    [ADD] customerマイページの編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,15 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def create
+    @customers = Customer.all
+    @customer = Customer.new(user_params)
+  if @customer.save
+    puts "保存に成功しました"
+  else
+    puts "保存に失敗しました"
+  end
+  end
 end
