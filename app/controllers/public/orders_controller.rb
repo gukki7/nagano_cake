9cef3b15bd8af58dae81fe3ff18fabd46833cd6c
EC2 Default User <duffy.may08@outlook.jp>
2022-08-09 14:27:30 +0000

    [ADD customerのカート画面編集

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,7 +15,11 @@ class Public::OrdersController < ApplicationController
 	end
 
 	def comfirm
-		customer = current_customer
+	 @order = Order.new(order_params)
+	 @address = Address.find(params[:order][:address_id])
+	 @order.postal_code = @address.postal_code
+     @order.address = @address.address
+     @order.name = @address.name
 		session[:order] = Order.new
 
 		cart_items = current_customer.cart_items
@@ -53,7 +57,37 @@ class Public::OrdersController < ApplicationController
 			session[:order][:name] = params[:name]
 		end
     	@cart_items = CartItem.all
+
 	end
+	
+	def complete
+		order = Order.new(session[:order])
+		order.save
+
+		if session[:new_address]
+			address = current_customer.addresses.new
+			address.postal_code = order.postal_code
+			address.address = order.address
+			address.name = order.name
+			address.save
+			session[:new_address] = nil
+		end
+
+		cart_items = current_customer.cart_items
+		cart_items.each do |cart_item|
+			order_detail = OrderDetail.new
+			order_detail.order_id = order.id
+			order_detail.item_id = cart_item.item.id
+			order_detail.amount = cart_item.amount
+			order_detail.making_status = 0
+			order_detail.price = cart_item.item.price
+			order_detail.save
+		end
+
+		# 購入後はカート内商品削除
+		cart_items.destroy_all
+	end
+	
 
 	def create
     	@orders = Order.all
@@ -70,10 +104,33 @@ class Public::OrdersController < ApplicationController
 			redirect_to public_orders_complete_path
 		end
 	end
+	
+	def create_order
+    # オーダーの保存
+    @order = Order.new
+    @order.customer_id = current_customer.id
+    @order.address = session[:address]
+    @order.payment = session[:payment]
+    @order.total_price = calculate(current_customer)
+    @order.order_status = 0
+    @order.save
+    # saveができた段階でOrderモデルにorder_idが入る
+
+    # オーダー商品ごとの詳細の保存
+	  current_customer.cart_items.each do |cart|
+      @order_detail = OrderDetail.new
+      @order_detail.order_id = @order.id
+      @order_detail.item_name = cart.item.name
+      @order_detail.item_price = cart.item.price
+      @order_detail.amount = cart.amount
+      @order_detail.item_status = 0
+      @order_detail.save
+	  end
+    end
 
 	private
-    def oredr_params
-      params.require(:order).permit(:address, :total_payment, :status)
+    def order_params
+      params.require(:order).permit(:address, :postal_code, :name, :total_payment, :status)
     end
 
 end
